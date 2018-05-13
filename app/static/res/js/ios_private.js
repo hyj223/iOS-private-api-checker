4117c6e079108ab79580b22ee0f0a33c368116ef
iHTCboy <ihetiancong@gmail.com>
2018-05-14 00:02:54 +0800

    增加检查详细列表结果显示。

--- app/static/res/js/ios_private.js
+++ app/static/res/js/ios_private.js
@@ -1,7 +1,7 @@
 Dropzone.autoDiscover = false;
 var myDropzone = new Dropzone("#ipa_file", {
 	url: "/ipa_post",
-	maxFilesize: 1024,
+	maxFilesize: 2048,
 	acceptedFiles: '.ipa',
 	maxFiles: 5,
 	success: function(d, data) {
@@ -9,35 +9,35 @@ var myDropzone = new Dropzone("#ipa_file", {
 		if (data.success == 1) {
 			//显示app信息
 			$('#app_name').text(data.name);
-			$('#app_version').text(data.version.originResult);
-			$('#app_build_version').text(data.build_version.originResult);
-			$('#bundle_identifier').text(data.bundle_id.originResult);
-			$('#device_family').text(data.device_family.reviewResult);
-			$('#ipa_filesize').text(data.ipa_filesize);
-			$('#development_region').text(data.development_region.originResult);
-			$('#target_os_version').text(data.tar_version.reviewResult);
-			$('#minimum_os_version').text(data.min_version.reviewResult);
+//			$('#app_version').text(data.version.originResult);
+//			$('#app_build_version').text(data.build_version.originResult);
+//			$('#bundle_identifier').text(data.bundle_id.originResult);
+//			$('#device_family').text(data.device_family.reviewResult);
+//			$('#ipa_filesize').text(data.ipa_filesize);
+//			$('#development_region').text(data.development_region.originResult);
+//			$('#target_os_version').text(data.tar_version.reviewResult);
+//			$('#minimum_os_version').text(data.min_version.reviewResult);
 			//显示ipa的架构信息
-			$('#app_arcs').text(data.arcs.originResult.join(' / '));
-			$('#profile_type').text(data.profile_type.reviewResult);
-			$('#expiration').text(data.expiration);
+//			$('#app_arcs').text(data.arcs.originResult.join(' / '));
+//			$('#profile_type').text(data.profile_type.reviewResult);
+//			$('#expiration').text(data.expiration);
 			//显示私有api信息
-			$('#api_in_app div.api_section').remove();
-			for (var i = 0; i < data.methods_in_app.length; i++) {
-				var api = data.methods_in_app[i];
-				var html = '<div class="api_section section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone">' +
-                  '<h5>' + (i + 1) + '、' + api.api_name + '</h5>' +
-                  'api is ' + api.type + ', IN sdk ' + api.sdk + '、' + api.framework + ' -> ' + api.header_file + ' -> ' + api.class_name + ' -> '+ api.api_name +
-                '</div>';
-                $('#api_append_div').append(html);
-			};
-			$('#framework_in_app div.api_section').remove();
-			for (var i = 0; i < data.private_framework.length; i++) {
-				var framework = data.private_framework[i];
-				var html = '<div class="api_section section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone">' +
-                  '<h5>' + (i + 1) + '、' + framework + '</h5>' +
-                '</div>';
-                $('#framework_append_div').append(html);
+//			$('#api_in_app div.api_section').remove();
+//			for (var i = 0; i < data.methods_in_app.length; i++) {
+//				var api = data.methods_in_app[i];
+//				var html = '<div class="api_section section__text mdl-cell mdl-cell--10-col-desktop mdl-cell--6-col-tablet mdl-cell--3-col-phone">' +
+//                  '<h5>' + (i + 1) + '、' + api.api_name + '</h5>' +
+//                  'api is ' + api.type + ', IN sdk ' + api.sdk + '、' + api.framework + ' -> ' + api.header_file + ' -> ' + api.class_name + ' -> '+ api.api_name +
+//                '</div>';
+//                $('#api_append_div').append(html);
+//			};
+			$("#checkInfoList tbody").html("");
+			for (var i = 0; i < data.checkResult.length; i++) {
+				var result = data.checkResult[i];
+				var tr = '<tr><td>' + (i + 1) + '</td><td>' +  result.status + '</td><td>' + result.reviewItem + '</td><td>'
+				  + result.originResult + '</td><td>' + result.reviewResult + '</td></tr>';
+				console.log(tr);
+                $('#checkInfoList tbody').append(tr);
 			};
 		}
 		else {
--- app/utils/EGKReviewResultUtil.py
+++ app/utils/EGKReviewResultUtil.py
@@ -10,17 +10,19 @@ Copyright © 2018 iHTCboy. All rights reserved.
 
 
 def handleReviewResult(data):
+    result = []
+
     if 'development_region' in data:
-        data.update(resultModel('development_region', '本地化语言(备选)', 'pass', data['development_region'], '本地化语言。如果应用不支持⽤户所在地相应的语言资源，则用这个值来作为默认'))
+        result.append(resultModel('development_region', '本地化语言(备选)', 'pass', data['development_region'], '本地化语言。如果应用不支持⽤户所在地相应的语言资源，则用这个值来作为默认'))
 
     if 'version' in data:
-        data.update(resultModel('version', '发布版本号', 'pass', data['version'], '⾯向用户市场的版本号，AppStore显示的版本号'))
+        result.append(resultModel('version', '发布版本号', 'pass', data['version'], '⾯向用户市场的版本号，AppStore显示的版本号'))
 
     if 'build_version' in data:
-        data.update(resultModel('build_version', '内部版本号', 'pass', data['build_version'], '应⽤内部版本号。用以记录开发版本，每次更新的时候都需要比上一次高。'))
+        result.append(resultModel('build_version', '内部版本号', 'pass', data['build_version'], '应⽤内部版本号。用以记录开发版本，每次更新的时候都需要比上一次高。'))
 
     if 'bundle_id' in data:
-        data.update(resultModel('bundle_id', '套装 ID', 'pass', data['bundle_id'], '应用唯一标识字符串，自行检查是否正确。'))
+        result.append(resultModel('bundle_id', '套装 ID', 'pass', data['bundle_id'], '应用唯一标识字符串，自行检查是否正确。'))
 
     if 'device_family' in data:
         devices = data['device_family']
@@ -38,42 +40,46 @@ def handleReviewResult(data):
             status = 'error'
             message = '验收人员注意：该应用支持的设备类型有误，联系技术人员检查'
 
-        data.update(resultModel('device_family', '检查支持设备种类', status, devices, message))
+        result.append(resultModel('device_family', '检查支持设备种类', status, devices, message))
 
     if 'tar_version' in data:
         target = data['tar_version']
         if float(target) >= 11 :
-            data.update(resultModel('tar_version', '构建应用的SDK版本', 'pass', target, '符合苹果要求使用 iOS11 SDK的规定'))
+            result.append(resultModel('tar_version', '构建应用的SDK版本', 'pass', target, '符合苹果要求使用 iOS11 SDK的规定'))
         else:
-            data.update(resultModel('tar_version', '构建应用的SDK版本', 'error', target, '应用不符合苹果要求使用 iOS11 SDK的规定！'))
+            result.append(resultModel('tar_version', '构建应用的SDK版本', 'error', target, '应用不符合苹果要求使用 iOS11 SDK的规定！'))
 
     if 'min_version' in data:
-        data.update(resultModel('min_version', '支持最低iOS版本', 'pass', data['min_version'], '验收人员注意：该应用只支持 %s 以上iOS系统版本' % data['min_version']))
+        result.append(resultModel('min_version', '支持最低iOS版本', 'pass', data['min_version'], '验收人员注意：该应用只支持 %s 以上iOS系统版本' % data['min_version']))
 
     if 'arcs' in data:
         architecture = data['arcs']
         if 'arm64' in architecture:
-            data.update(resultModel('arcs', '设备支持的架构', 'pass', data['arcs'], '包括 arm64 架构指令，符合苹果规则'))
+            result.append(resultModel('arcs', '设备支持的架构', 'pass', data['arcs'], '包括 arm64 架构指令，符合苹果规则'))
         else:
-            data.update(resultModel('arcs', '设备支持的架构', 'error', data['arcs'], '验收人员注意：应用不支持 arm64 架构指令，不符合苹果规则要求！'))
+            result.append(resultModel('arcs', '设备支持的架构', 'error', data['arcs'], '验收人员注意：应用不支持 arm64 架构指令，不符合苹果规则要求！'))
 
     if 'profile_type' in data:
         type = data['profile_type']
         if type == 'Enterprise':
-            data.update(resultModel('profile_type', '检查证书是否符合规范', 'pass', type, '检查出该证书为 %s' % type))
+            result.append(resultModel('profile_type', '检查证书是否符合规范', 'pass', type, '检查出该证书为 %s' % type))
         else:
-            data.update(resultModel('profile_type', '检查证书是否符合规范', 'warn', type, '警告：检查出该证书为 %s，仅用于内部测试使用。建议：提交iTC则需要打包发布证书！' % type))
+            result.append(resultModel('profile_type', '检查证书是否符合规范', 'warn', type, '警告：检查出该证书为 %s，仅用于内部测试使用。建议：提交iTC则需要打包发布证书！' % type))
+
+    data['checkResult'] = result
 
     return data
 
 
 def resultModel(name, reviewItem, status, originResult, reviewResult):
     result = {}
+    result[name] = name
     result['reviewItem'] = reviewItem
     result['status'] = status
     result['originResult'] = originResult
     result['reviewResult'] = reviewResult
-    return {name: result}
+    return result
+
 
 
 
